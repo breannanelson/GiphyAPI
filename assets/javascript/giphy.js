f90e3533947fa43469f166e2a17c866c67beba16
Breanna Nelson <breanna_a_nelson@hotmail.com>
2018-02-26 17:43:27 -0800

    still and animate gif configs

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,45 +1,89 @@
 
 var carArr = ["Corvette", "Mustang", "Chevrolet Camaro", "Dodge Charger", "Shelby Cobra"];
 
-var btnCount = 0;
+var carCount = 0;
 
-while (btnCount < carArr.length) {
-    $("#buttons").append("<button id='btn-" + (i + 1) + "' >" + carArr[btnCount] + "</button>");
-    btnCount++;
+for (var i = 0; i < carArr.length; i++) {
+    var a = $("<button>");
+    // Adding a class of car-btn to our button
+    a.addClass("car-btn");
+    // Adding a data-attribute
+    a.attr("data-name", carArr[i]);
+    // Providing the initial button text
+    a.text(carArr[i]);
+
+    $("#buttons-view").append(a);
+    carCount++;
 }
+
 
 $("#search-btn").on("click", function () {
 
     var searchTerm = $("#searched-val").val();
     carArr.push(searchTerm);
-    console.log(carArr)
 
-    $("#buttons").append("<button id='btn-" + (btnCount + 1) + "' >" + searchTerm + "</button>");
+    var a = $("<button>");
+    // Adding a class of car-btn to our button
+    a.addClass("car-btn");
+    // Adding a data-attribute
+    a.attr("data-name", carArr[carCount]);
+    // Providing the initial button text
+    a.text(carArr[carCount]);
+
+    $("#buttons-view").append(a);
 
     $("#searched-val").val("");
+
+    carCount++;
+
     event.preventDefault()
-    btnCount++;                 
 })
 
-for(var i = 0; i < carArr.length; i++){
-    $("#btn-" + i).on("click", function () {
-        // Here we construct our URL
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=mejJaFWSR2V2aY31O2HJaYqpl1cqLjBn&q=" + searchTerm + "&limit=15&offset=0&rating=G&lang=en"
-    
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(function (obj) {
-            console.log(obj)
-            //image
-            for (var i = 0; i <= 15; i++) {
-                $("#gif").append("<img src='obj.data[i].images['480w_still'].url'>")
-            }
-    
-    
-            //rating
-        })
+$(document).on("click", ".car-btn", function () {
 
-})
-}
-// $(this).
+    $("#gif").empty();
+
+    var searchTerm = $(this).attr("data-name");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=mejJaFWSR2V2aY31O2HJaYqpl1cqLjBn&q=" + searchTerm + "&limit=15&offset=0&rating=G&lang=en"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (obj) {
+        console.log(obj)
+        //image
+        for (var i = 0; i <= 15; i++) {
+            $("#gif").append("<img class='pic' " +
+                "src='" + obj.data[i].images["480w_still"].url + "'" +
+                "data-still='" + obj.data[i].images["480w_still"].url + "'" +
+                "data-animate='" + obj.data[i].images.fixed_height.url + "'" +
+                "alt='Gif of " + searchTerm + "' data-state='still' style='width:300px;'>")
+            $("#gif").append(obj.data[i].rating)
+        }
+
+
+    })
+
+});
+
+
+$(document).on("click", ".pic", function () {
+    var state = $(this).attr("data-state")
+
+    if (state === "still") {
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+    }
+    else {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));
+    }
+
+});
+
+
+
+
+
+
